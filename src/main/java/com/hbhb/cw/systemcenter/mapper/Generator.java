c5714ab275bb8ee20f1cd1038df8590a6b40bdb8
xiaokang <13148807065@163.com>
2020-10-16 09:22:05 +0800

    部署

--- src/main/java/com/hbhb/cw/systemcenter/mapper/Generator.java
+++ src/main/java/com/hbhb/cw/systemcenter/mapper/Generator.java
@@ -1,31 +1,40 @@
-package com.hbhb.cw.systemcenter.mapper;
-
-import org.beetl.core.GroupTemplate;
-import org.beetl.core.resource.ClasspathResourceLoader;
-import org.beetl.sql.core.SQLManager;
-import org.beetl.sql.gen.simple.BaseTemplateSourceBuilder;
-
-/**
- * @author dxk
- */
-public class Generator {
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://mariadb.yeexun.com.cn/cw_sys";
-    private static final String USERNAME = "zhcw";
-    private static final String PASSWORD = "zhcw@2020";
-    /**
-     * 数据库表名
-     */
-    private static final String TABLE_NAME = "sys_file";
-
-    protected void initGroupTemplate() {
-        GroupTemplate groupTemplate = BaseTemplateSourceBuilder.getGroupTemplate();
-        ClasspathResourceLoader resourceLoader = new ClasspathResourceLoader("gens/");
-        groupTemplate.setResourceLoader(resourceLoader);
-    }
-
-    public static void main(String[] args) {
-        SQLManager sqlManager = SQLManager.newBuilder(DRIVER, URL, USERNAME, PASSWORD).build();
+//package com.hbhb.cw.systemcenter.mapper;
+//
+//import org.beetl.core.GroupTemplate;
+//import org.beetl.core.ReThrowConsoleErrorHandler;
+//import org.beetl.core.resource.ClasspathResourceLoader;
+//import org.beetl.sql.core.SQLManager;
+//import org.beetl.sql.gen.SourceBuilder;
+//import org.beetl.sql.gen.SourceConfig;
+//import org.beetl.sql.gen.simple.BaseTemplateSourceBuilder;
+//import org.beetl.sql.gen.simple.ConsoleOnlyProject;
+//import org.beetl.sql.gen.simple.EntitySourceBuilder;
+//import org.beetl.sql.gen.simple.MapperSourceBuilder;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * @author dxk
+// */
+//public class Generator {
+//    private static final String DRIVER = "com.mysql.jdbc.Driver";
+//    private static final String URL = "jdbc:mysql://mariadb.yeexun.com.cn/cw_sys";
+//    private static final String USERNAME = "zhcw";
+//    private static final String PASSWORD = "zhcw@2020";
+//    /**
+//     * 数据库表名
+//     */
+//    private static final String TABLE_NAME = "sys_file";
+//
+//    protected void initGroupTemplate() {
+//        GroupTemplate groupTemplate = BaseTemplateSourceBuilder.getGroupTemplate();
+//        ClasspathResourceLoader resourceLoader = new ClasspathResourceLoader("gens/");
+//        groupTemplate.setResourceLoader(resourceLoader);
+//    }
+//
+//    public static void main(String[] args) {
+//        SQLManager sqlManager = SQLManager.newBuilder(DRIVER, URL, USERNAME, PASSWORD).build();
 //        List<SourceBuilder> sourceBuilder = new ArrayList<>();
 //        sourceBuilder.add(new EntitySourceBuilder());
 //        sourceBuilder.add(new MapperSourceBuilder());
@@ -35,7 +44,5 @@ public class Generator {
 //        ConsoleOnlyProject project = new ConsoleOnlyProject();
 ////        SimpleMavenProject project = new SimpleMavenProject();
 //        config.gen(TABLE_NAME, project);
-    }
-
-    String daoPkg = "com.hbhb.cw.systemcenter.mapper";
-}
+//    }
+//}
