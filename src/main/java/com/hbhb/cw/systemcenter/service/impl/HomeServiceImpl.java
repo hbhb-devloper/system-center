9c626140856b1289514803c589efb9f3af688226
wangxiaogang <15151269@163.com>
2021-01-11 21:03:26 +0800

    密码修改

--- src/main/java/com/hbhb/cw/systemcenter/service/impl/HomeServiceImpl.java
+++ src/main/java/com/hbhb/cw/systemcenter/service/impl/HomeServiceImpl.java
@@ -6,11 +6,6 @@ import com.hbhb.cw.systemcenter.enums.TypeCode;
 import com.hbhb.cw.systemcenter.rpc.BudgetApiExp;
 import com.hbhb.cw.systemcenter.rpc.FlowApiExp;
 import com.hbhb.cw.systemcenter.rpc.FundApiExp;
-import com.hbhb.cw.systemcenter.rpc.PublicityApplicationApiExp;
-import com.hbhb.cw.systemcenter.rpc.PublicityMaterialsApiExp;
-import com.hbhb.cw.systemcenter.rpc.PublicityPictureApiExp;
-import com.hbhb.cw.systemcenter.rpc.PublicityPrintApiExp;
-import com.hbhb.cw.systemcenter.rpc.PublicityVerifyApiExp;
 import com.hbhb.cw.systemcenter.rpc.WarnApiExp;
 import com.hbhb.cw.systemcenter.service.HomeService;
 import com.hbhb.cw.systemcenter.service.SysDictService;
@@ -18,17 +13,14 @@ import com.hbhb.cw.systemcenter.service.SysUserService;
 import com.hbhb.cw.systemcenter.vo.DictVO;
 import com.hbhb.cw.systemcenter.vo.UserInfo;
 import com.hbhb.cw.systemcenter.web.vo.HomeModuleVO;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import javax.annotation.Resource;
-
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author wxg
@@ -50,16 +42,16 @@ public class HomeServiceImpl implements HomeService {
     private WarnApiExp warnApiExp;
     @Resource
     private FlowApiExp flowApi;
-    @Resource
-    private PublicityMaterialsApiExp materialsApi;
-    @Resource
-    private PublicityPictureApiExp pictureApi;
-    @Resource
-    private PublicityPrintApiExp printApi;
-    @Resource
-    private PublicityApplicationApiExp applicationApi;
-    @Resource
-    private PublicityVerifyApiExp verifyApi;
+//    @Resource
+//    private PublicityMaterialsApiExp materialsApi;
+//    @Resource
+//    private PublicityPictureApiExp pictureApi;
+//    @Resource
+//    private PublicityPrintApiExp printApi;
+//    @Resource
+//    private PublicityApplicationApiExp applicationApi;
+//    @Resource
+//    private PublicityVerifyApiExp verifyApi;
 
     @Override
     public List<HomeModuleVO> getModuleList(Integer userId) {
@@ -93,17 +85,17 @@ public class HomeServiceImpl implements HomeService {
             workList.add(module3);
         }
 
-        // 宣传用品提醒统计
-        HomeModuleVO module4 = new HomeModuleVO();
-        module4.setModule(Module.MODULE_PUBLICITY.getValue());
-        module4.setModuleName(moduleMap.get(Module.MODULE_PUBLICITY.getValue().toString()));
-        Long count = pictureApi.countNotice(userId);
-        Long count1 = printApi.countNotice(userId);
-        Long count2 = materialsApi.countNotice(userId);
-        Long count3 = applicationApi.countNotice(userId);
-        Long count4 = verifyApi.countNotice(userId);
-        module4.setCount(count + count1 + count2 + count3 + count4);
-        workList.add(module4);
+//        // 宣传用品提醒统计
+//        HomeModuleVO module4 = new HomeModuleVO();
+//        module4.setModule(Module.MODULE_PUBLICITY.getValue());
+//        module4.setModuleName(moduleMap.get(Module.MODULE_PUBLICITY.getValue().toString()));
+//        Long count = pictureApi.countNotice(userId);
+//        Long count1 = printApi.countNotice(userId);
+//        Long count2 = materialsApi.countNotice(userId);
+//        Long count3 = applicationApi.countNotice(userId);
+//        Long count4 = verifyApi.countNotice(userId);
+//        module4.setCount(count + count1 + count2 + count3 + count4);
+//        workList.add(module4);
         return workList;
     }
 }
