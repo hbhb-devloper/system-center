7810ef2def286dfc7e0d02239ea248ca949df991
wangxiaogang <15151269@163.com>
2021-02-27 16:51:53 +0800

    2021.2.27 用户密码修改

--- src/main/java/com/hbhb/cw/systemcenter/web/controller/SysUserController.java
+++ src/main/java/com/hbhb/cw/systemcenter/web/controller/SysUserController.java
@@ -258,7 +258,6 @@ public class SysUserController implements UserApi {
     @Override
     public UserInfo getUserInfoByEmail(@Parameter(required = true) String email) {
         return sysUserService.getEmail(email);
-
     }
 
 }
